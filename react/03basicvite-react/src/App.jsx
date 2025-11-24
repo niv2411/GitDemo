2553ff515a390a63f606c1d25b0f893f28515ad8
Nivaan Srivastava <nivaansrivastava@Nivaans-MacBook-Air.local>
2025-11-24 16:22:44 +0530

    Ghost rider

---
+++ react/03basicvite-react/src/App.jsx
@@ -0,0 +1,17 @@
+import DummyCompo from "./dummyCompo";
+
+function App() {
+  
+
+
+  return (
+    <>
+     <DummyCompo />
+     <h1>Hello World | vite</h1>
+     <p>Hey it's a paragraph</p>
+    </>
+
+  )
+}
+
+export default App
